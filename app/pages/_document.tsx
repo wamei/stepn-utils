2c8dca8c6f4753cd91a920a036d0739396bb098d
wamei <wamei.cho@gmail.com>
2022-06-08 13:19:59 +0900

    add ga

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -1,4 +1,4 @@
-import { Document, Html, DocumentHead, Main, BlitzScript /*DocumentContext*/, Head } from 'blitz'
+import { Document, Html, DocumentHead, Main, BlitzScript /*DocumentContext*/ } from 'blitz'
 import React from 'react'
 
 class MyDocument extends Document {
@@ -17,6 +17,18 @@ class MyDocument extends Document {
           <meta name='description' content='STEPN役立ちツール' />
         </DocumentHead>
         <body>
+          <script
+            type='text/javascript'
+            dangerouslySetInnerHTML={{
+              __html: `
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W2TWG4C');
+`,
+            }}
+          />
           <Main />
           <BlitzScript />
         </body>
